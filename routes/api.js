8af16a167c5896ef5ef6212075fd53c6f468d919
Furkan Emin <femincanx@gmail.com>
2023-01-15 09:11:47 +0300

    feat: add /convert route

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,35 @@
 'use strict';
 
-const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
   let convertHandler = new ConvertHandler();
 
+  app.get('/api/convert', (req, res) => {
+    const { input } = req.query;
+
+    const num = convertHandler.getNum(input);
+    const unit = convertHandler.getUnit(input);
+
+    if (num === 'invalid number' && unit === 'invalid unit') {
+      return res.send('invalid number and unit');
+    }
+
+    if (num === 'invalid number') {
+      return res.send(num);
+    }
+
+    if (unit === 'invalid unit') {
+      return res.send(unit);
+    }
+
+    res.json(
+      convertHandler.getString(
+        num,
+        unit,
+        convertHandler.convert(num, unit),
+        convertHandler.getReturnUnit(unit)
+      )
+    );
+  });
 };
